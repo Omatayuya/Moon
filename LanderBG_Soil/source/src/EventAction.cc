6e4b5511a1b40ed96d921af7788a45befb73a5f9
Yuya Omata <omata.yuya.83w@st.kyoto-u.ac.jp>
2026-08-27 14:47:53 +0900

    test2: フック修正後の動作確認

--- LanderBG_Soil/source/src/EventAction.cc
+++ LanderBG_Soil/source/src/EventAction.cc
@@ -267,7 +267,5 @@ G4int EventAction::ProcessID(G4String process)
 		// G4cout << "other process: " << process << G4endl;
 	}
 
-	std::cout << "Process: " << process << ", ID: " << id << std::endl;
-
 	return id;
 }
